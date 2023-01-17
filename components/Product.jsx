5993a84c5aa1b543b33d8fda4cd1a8f715094b28
William M. Tsikata <tsikatawill@gmail.com>
2023-01-17 03:43:16 +0000

    populate product card

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,3 +1,23 @@
-export const Product = () => {
-  return <div>Product</div>;
+import Image from "next/image";
+import Link from "next/link";
+import { urlFor } from "@/lib/client";
+
+export const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <Image
+            src={urlFor(image[0])}
+            width={250}
+            height={250}
+            alt={name}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">Ghc{parseFloat(price).toFixed(2)}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
