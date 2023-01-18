91a50e201398892ad79d93116b18f1360cc9cfc5
William M. Tsikata <tsikatawill@gmail.com>
2023-01-18 07:11:56 +0000

    populate footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,16 @@
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>&copy; 2022 Willcommerce All Rights Reserved</p>
+
+        <div className="icons">
+          <AiFillInstagram />
+          <AiOutlineTwitter />
+        </div>
+      </div>
+    </footer>
+  );
 };
