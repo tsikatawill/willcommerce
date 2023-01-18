d3f787bf32dbdd3923c375a21111c8d9511a96fb
William M. Tsikata <tsikatawill@gmail.com>
2023-01-18 07:13:01 +0000

    populate navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,17 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import Link from "next/link";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Willcommerce</Link>
+      </p>
+
+      <button className="cart-icon">
+        <AiOutlineShoppingCart />
+        <span className="cart-item-qty">3</span>
+      </button>
+    </div>
+  );
 };
